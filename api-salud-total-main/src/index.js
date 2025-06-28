330403b5318036ce23e5540df1c2f2e19940092b
JonathanAranedaa <jo.aranedam@duocuc.cl>
2025-06-28 00:59:29 -0400

    Delete api-salud-total-main directory

--- api-salud-total-main/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import app from './app.js';
-console.log('RESEND_API_KEY:', process.env.RESEND_API_KEY);
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
